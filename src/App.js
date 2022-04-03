445fe630a364bf5b3d3810a699ac705ef705bfd2
Praveen Kumar Soni <86279438+praveen-soni-by@users.noreply.github.com>
2022-04-03 14:59:22 +0530

    Update README.md

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "primeicons/primeicons.css";
 import "primereact/resources/primereact.min.css";
+import "primeflex/primeflex.min.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "./App.css";
-import Category from './pages/Category';
+import Layout from './components/Layout';
 
 function App() {
-  return <Category/>
+  return <Layout/>
 }
 
 export default App;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,136 +1,181 @@
+import React, { useState, useEffect, useRef } from "react";
+import { DataView, DataViewLayoutOptions } from "primereact/dataview";
+import { Rating } from "primereact/rating";
+import { Button } from "primereact/button";
+import { DATA } from "./data";
+import "./layout.css";
 
-import React, { useState, useEffect, useRef } from 'react';
-import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
-import { Rating } from 'primereact/rating';
-import { Button } from 'primereact/button';
-import {DATA} from './data'
-import './layout.css';
+export default function Layout() {
+  const [products, setProducts] = useState(null);
+  const [layout, setLayout] = useState("grid");
+  const [loading, setLoading] = useState(true);
+  const [first, setFirst] = useState(0);
+  const [totalRecords, setTotalRecords] = useState(0);
+  const rows = useRef(12);
+  const datasource = useRef([]);
+  const isMounted = useRef(false);
 
-export default function Layout () {
-    const [products, setProducts] = useState(null);
-    const [layout, setLayout] = useState('grid');
-    const [loading, setLoading] = useState(true);
-    const [first, setFirst] = useState(0);
-    const [totalRecords, setTotalRecords] = useState(0);
-    const rows = useRef(6);
-    const datasource = useRef(null);
-    const isMounted = useRef(false);
-
-    useEffect(() => {
-        if (isMounted.current) {
-            setTimeout(() => {
-                setLoading(false);
-            }, 1000);
-        }
-    }, [loading]); // eslint-disable-line react-hooks/exhaustive-deps
-
-    useEffect(() => {
-        setTimeout(() => {
-            isMounted.current = true;
-                datasource.current = DATA;
-                setTotalRecords(DATA.length);
-                setProducts(datasource.current.slice(0, rows.current));
-                setLoading(false);
-          
-        }, 1000);
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-    const onPage = (event) => {
-        setLoading(true);
-
-        //imitate delay of a backend call
-        setTimeout(() => {
-            const startIndex = event.first;
-            const endIndex = Math.min(event.first + rows.current, totalRecords - 1);
-            const newProducts = startIndex === endIndex ? datasource.slice(startIndex) : datasource.slice(startIndex, endIndex);
-
-            setFirst(startIndex);
-            setProducts(newProducts);
-            setLoading(false);
-        }, 1000);
+  useEffect(() => {
+    if (isMounted.current) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
     }
+  }, [loading]);
 
-    const renderListItem = (data) => {
-        return (
-            <div className="col-12">
-                <div className="product-list-item">
-                    <img src={`images/product/${data.image}`} onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} alt={data.name} />
-                    <div className="product-list-detail">
-                        <div className="product-name">{data.name}</div>
-                        <div className="product-description">{data.description}</div>
-                        <Rating value={data.rating} readOnly cancel={false}></Rating>
-                        <i className="pi pi-tag product-category-icon"></i><span className="product-category">{data.category}</span>
-                    </div>
-                    <div className="product-list-action">
-                        <span className="product-price">${data.price}</span>
-                        <Button icon="pi pi-shopping-cart" label="Add to Cart" disabled={data.inventoryStatus === 'OUTOFSTOCK'}></Button>
-                        <span className={`product-badge status-${data.inventoryStatus.toLowerCase()}`}>{data.inventoryStatus}</span>
-                    </div>
-                </div>
+  useEffect(() => {
+    setTimeout(() => {
+      isMounted.current = true;
+      datasource.current = DATA;
+      setTotalRecords(DATA.length);
+      setProducts(datasource.current.slice(0, rows.current));
+      setLoading(false);
+    }, 1000);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const onPage = (event) => {
+    setLoading(true);
+    //imitate delay of a backend call
+    setTimeout(() => {
+      const startIndex = event.first;
+      const endIndex = Math.min(event.first + rows.current, totalRecords - 1);
+
+       let newProducts =
+        startIndex === endIndex
+          ? datasource.current.slice(startIndex)
+          : datasource.current.slice(startIndex, endIndex);
+      setFirst(startIndex);
+      setProducts(newProducts);
+      setLoading(false);
+    }, 1000);
+  };
+
+  const renderListItem = (data) => {
+    return (
+      <div className="col-12">
+        <div className="product-list-item">
+          <img
+            src={`images/product/${data.image}`}
+            onError={(e) =>
+              (e.target.src =
+                "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
+            }
+            alt={data.name}
+          />
+          <div className="product-list-detail">
+            <div className="product-name">{data.name}</div>
+            <Rating value={data.rating} readOnly cancel={false}></Rating>
+            <i className="pi pi-tag product-category-icon"></i>
+            <span className="product-category">{data.category}</span>
+          </div>
+          <div className="product-list-action ">
+            <span className="product-price">${data.price}</span>
+            <Button
+              className="p-button-text"
+              icon="pi pi-shopping-cart"
+              label="Add to Cart"
+              disabled={data.inventoryStatus === "OUTOFSTOCK"}
+            ></Button>
+            <span
+              className={`product-badge status-${data.inventoryStatus.toLowerCase()}`}
+            >
+              {data.inventoryStatus}
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderGridItem = (data) => {
+    return (
+      <div className="p-col-12 p-md-6 p-lg-3">
+        <div className="product-grid-item card">
+          <div className="product-grid-item-top">
+            <div>
+              <i className="pi pi-tag product-category-icon"></i>
+              <span className="product-category">{data.category}</span>
             </div>
-        );
+            <span
+              className={`product-badge status-${data.inventoryStatus.toLowerCase()}`}
+            >
+              {data.inventoryStatus}
+            </span>
+          </div>
+          <div className="product-grid-item-content">
+            <img
+              src={`https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MKUQ3_VW_34FR+watch-45-alum-midnight-nc-7s_VW_34FR_WF_CO?wid=750&hei=712&trim=1,0&fmt=p-jpg&qlt=95&.v=1632171067000,1631661671000`}
+              onError={(e) =>
+                (e.target.src =
+                  "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
+              }
+              alt={data.name}
+            />
+            <div className="product-name">{data.name}</div>
+            <Rating
+              className="mb-2"
+              value={data.rating}
+              readOnly
+              cancel={false}
+            ></Rating>
+          </div>
+          <div className="product-grid-item-bottom">
+            <span className="product-price">${data.price}</span>
+            <Button
+              icon="pi pi-shopping-cart"
+              className="p-button-text"
+              label="Add to Cart"
+              disabled={data.inventoryStatus === "OUTOFSTOCK"}
+            ></Button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const itemTemplate = (product, layout) => {
+    if (!product) {
+      return;
     }
 
-    const renderGridItem = (data) => {
-        return (
-            <div className="col-12 md:col-4">
-                <div className="product-grid-item card">
-                    <div className="product-grid-item-top">
-                        <div>
-                            <i className="pi pi-tag product-category-icon"></i>
-                            <span className="product-category">{data.category}</span>
-                        </div>
-                        <span className={`product-badge status-${data.inventoryStatus.toLowerCase()}`}>{data.inventoryStatus}</span>
-                    </div>
-                    <div className="product-grid-item-content">
-                    <img src={`images/product/${data.image}`} onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} alt={data.name} />
-                        <div className="product-name">{data.name}</div>
-                        <div className="product-description">{data.description}</div>
-                        <Rating value={data.rating} readOnly cancel={false}></Rating>
-                    </div>
-                    <div className="product-grid-item-bottom">
-                        <span className="product-price">${data.price}</span>
-                        <Button icon="pi pi-shopping-cart" label="Add to Cart" disabled={data.inventoryStatus === 'OUTOFSTOCK'}></Button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+    if (layout === "list") return renderListItem(product);
+    else if (layout === "grid") return renderGridItem(product);
+  };
 
-    const itemTemplate = (product, layout) => {
-        if (!product) {
-            return;
-        }
-
-        if (layout === 'list')
-            return renderListItem(product);
-        else if (layout === 'grid')
-            return renderGridItem(product);
-    }
-
-    const renderHeader = () => {
-        let onOptionChange = (e) => {
-            setLoading(true);
-            setLayout(e.value);
-        };
-
-        return (
-            <div style={{ textAlign: 'left' }}>
-                <DataViewLayoutOptions layout={layout} onChange={onOptionChange} />
-            </div>
-        );
-    }
-
-    const header = renderHeader();
+  const renderHeader = () => {
+    let onOptionChange = (e) => {
+      setLoading(true);
+      setLayout(e.value);
+    };
 
     return (
-        <div className="dataview-demo">
-            <div className="card">
-                <DataView value={products} layout={layout} header={header}
-                        itemTemplate={itemTemplate} lazy paginator paginatorPosition={'both'} rows={rows.current}
-                        totalRecords={totalRecords} first={first} onPage={onPage} loading={loading} />
-            </div>
-        </div>
+      <div style={{ textAlign: "left" }}>
+        <DataViewLayoutOptions layout={layout} onChange={onOptionChange} />
+        <div className="grid"></div>
+      </div>
     );
+  };
+
+  const header = renderHeader();
+
+  return (
+    <div className="dataview-demo">
+      <div className="card">
+        <DataView
+          value={products}
+          layout={layout}
+          header={header}
+          itemTemplate={itemTemplate}
+          lazy
+          paginator
+          paginatorPosition={"bottom"}
+          rows={rows.current}
+          totalRecords={totalRecords}
+          first={first}
+          onPage={onPage}
+          loading={loading}
+        />
+      </div>
+    </div>
+  );
 }
-                 
